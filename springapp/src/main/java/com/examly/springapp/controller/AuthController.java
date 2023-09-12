83f582c0920fbfaaa35f46edeeb3201b06c8f18b
Iamneo <techadmin@iamneo.ai>
2023-09-12 15:56:59 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-@CrossOrigin(origins = "https://8080-beacfdbedeedadecdcbbcffffaabdddabdebfcccb.premiumproject.examly.io")
+@CrossOrigin(origins = "https://8081-beacfdbedeedadecdcbbcffffaabdddabdebfcccb.premiumproject.examly.io")
 @RestController
 @RequestMapping("/auth")
 
