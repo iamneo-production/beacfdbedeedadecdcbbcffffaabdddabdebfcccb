6eeb1e4fa2a763dd8fccf7df8ffed10810d71334
Iamneo <techadmin@iamneo.ai>
2023-09-13 05:56:05 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/model/User.java
+++ springapp/src/main/java/com/examly/springapp/model/User.java
@@ -3,6 +3,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.CascadeType;
 
 @Entity
 public class User {
--- springapp/src/main/java/com/examly/springapp/service/AuthServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/AuthServiceImpl.java
@@ -4,6 +4,9 @@ import com.examly.springapp.model.User;
 import com.examly.springapp.repository.AuthRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.examly.springapp.repository.LoginRepository;
+import com.examly.springapp.model.Login;
+
 
 import java.util.List;
 import java.util.Optional;
