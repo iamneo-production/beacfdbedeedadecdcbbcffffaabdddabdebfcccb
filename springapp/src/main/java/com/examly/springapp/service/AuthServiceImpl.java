1cabb171177c96f8a7661b81cdb77d85ff3f7868
Iamneo <techadmin@iamneo.ai>
2023-09-13 08:11:04 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/service/AuthServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/AuthServiceImpl.java
@@ -8,9 +8,6 @@ import com.examly.springapp.model.Admin;
 import com.examly.springapp.repository.AdminRepository;
 import com.examly.springapp.repository.LoginRepository;
 import com.examly.springapp.repository.AuthRepository;
-
-
-
 import java.util.List;
 import java.util.Optional;
 
@@ -38,24 +35,31 @@ public class AuthServiceImpl implements AuthService { // Remove "abstract" keywo
         loginRepository.save(login);
         return savedUser;
     }
-    
-    public void duplicateAdmins() {
-        List<User> usersWithAdminRole = userRepository.findByUserRole("admin");
-        
-        for (User user : usersWithAdminRole) {
-            Admin admin = new Admin();
-            admin.setUsername(user.getUsername());
-            admin.setPassword(user.getPassword());
-            admin.setUserRole(user.getUserRole());
-            
-            adminRepository.save(admin);
-        }
-    }
 
-     @Override
+    @Override
      public User saveAdmin(User adminUser){
+        User savedAdmin = authRepository.save(adminUser);
+        Admin admin = new Admin();
          return authRepository.save(adminUser);
+         if (adminUser.getUserRole().equals("admin")){
+            
+         }
      }
+    
+    // public void duplicateAdmins() {
+    //     List<User> usersWithAdminRole = userRepository.findByUserRole("admin");
+        
+    //     for (User user : usersWithAdminRole) {
+    //         Admin admin = new Admin();
+    //         admin.setUsername(user.getUsername());
+    //         admin.setPassword(user.getPassword());
+    //         admin.setUserRole(user.getUserRole());
+            
+    //         adminRepository.save(admin);
+    //     }
+    // }
+
+     
 
     @Override
     public Optional<User> getUserByEmailAndPassword(String email, String password) {
